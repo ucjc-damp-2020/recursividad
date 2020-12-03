a63dff1aeda06e05fe50fe8534ac3d13c59424de
ASS <asolas@ucjc.edu>
2020-12-03 14:10:51 +0100

    commit clase

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 
+lista= [1,1,1,1,1]
+
+def sumatorio(list):
+    res = 0
+    for i in lista:
+        res+=i
+    return res
+
+
+def sumatorio_rec(list, longitud):
+    acum = 0
+    if longitud>1:
+        index = longitud-1
+        acum = sumatorio_rec(list,index)
+    return list[longitud-1] + acum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
